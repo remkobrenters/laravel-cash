4b5b13c6c7dd72fbd3c79df9df290b7f74acc6f6
Remko Brenters <remko.brenters@webparking.nl>
2020-05-12 11:36:45 +0200

    Added address details to Relation

--- src/Resources/Relation.php
+++ src/Resources/Relation.php
@@ -8,6 +8,9 @@ namespace Webparking\LaravelCash\Resources;
  * @property string $relationNumber 0101 Relatienummer
  * @property string $searchName     0102 Zoeknaam
  * @property string $name           0102 Naam
+ * @property string $address        0105 Adres
+ * @property string $postalCode     0438 Postcode
+ * @property string $city           1837 Woonplaats
  */
 class Relation extends BaseResource implements ResourceInterface
 {
@@ -15,6 +18,9 @@ class Relation extends BaseResource implements ResourceInterface
         'relationNumber' => '0101',
         'searchName' => '0102',
         'name' => '0103',
+        'address' => '0105',
+        'postal_code' => '0438',
+        'city' => '1837',
     ];
 
     public function getIdField(): string
@@ -30,5 +36,20 @@ class Relation extends BaseResource implements ResourceInterface
     public function getName(): ?string
     {
         return $this->attributes['name'];
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->attributes['address'];
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->attributes['postal_code'];
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->attributes['city'];
     }
 }
