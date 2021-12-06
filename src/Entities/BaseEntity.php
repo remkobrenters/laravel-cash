d6412c09984b38352779c59d17548d84c384d4d8
RVxLab <richard@rvx.works>
2021-12-06 15:35:50 +0100

    Add create method for resources
    Add test for index endpoint
    
    Separate import and export call handlers
    
    Begin implementation of creating delivery addresses

--- src/Entities/BaseEntity.php
+++ src/Entities/BaseEntity.php
@@ -7,6 +7,8 @@ namespace Webparking\LaravelCash\Entities;
 use Illuminate\Support\Collection;
 use Webparking\LaravelCash\Client;
 use Webparking\LaravelCash\Enums\DataType;
+use Webparking\LaravelCash\Exceptions\CashApiException;
+use Webparking\LaravelCash\Exceptions\CashValidationException;
 use Webparking\LaravelCash\Resources\BaseResource;
 
 abstract class BaseEntity
@@ -52,6 +54,11 @@ abstract class BaseEntity
         $this->call(DataType::IMPORT(), $this->endpoint, $resourceId, $resource->getSaveAttributes());
     }
 
+    public function create(BaseResource $resource): void
+    {
+        $this->call(DataType::IMPORT(), $this->endpoint, null, $resource->getSaveAttributes());
+    }
+
     protected function map(\SimpleXMLElement $xml): Collection
     {
         $collection = new Collection();
@@ -78,6 +85,36 @@ abstract class BaseEntity
             throw new \RuntimeException(sprintf('Call failed, response code %s received', $response['response']->code));
         }
 
+        if (DataType::EXPORT()->equals($dataType)) {
+            return $this->handleExportResponse($response);
+        }
+
+        if (DataType::IMPORT()->equals($dataType)) {
+            $this->handleImportResponse($response);
+        }
+
+        return null;
+    }
+
+    /**
+     * @throws CashApiException
+     */
+    protected function handleImportResponse(array $response): void
+    {
+        $status = data_get($response, 'transaction.status');
+        $errors = data_get($response, 'transaction.err');
+
+        if (!empty($errors)) {
+            $errorXml = $this->client->parseResponse($errors);
+
+            if (3 === $status) {
+                throw new CashValidationException($errorXml, $status);
+            }
+        }
+    }
+
+    protected function handleExportResponse(array $response): ?\SimpleXMLElement
+    {
         if (isset($response['exportResult'])) {
             $this->lastRawDataSet = $this->client->parseResponse($response['exportResult']);
 
--- tests/Feature/Entities/DeliveryAddressTest.php
+++ tests/Feature/Entities/DeliveryAddressTest.php
@@ -7,6 +7,8 @@ namespace Webparking\LaravelCash\Tests\Feature\Entities;
 use Artisaninweb\SoapWrapper\Service;
 use Artisaninweb\SoapWrapper\SoapWrapper;
 use Webparking\LaravelCash\Entities\DeliveryAddress;
+use Webparking\LaravelCash\Exceptions\CashApiException;
+use Webparking\LaravelCash\Resources\DeliveryAddress as DeliveryAddressResource;
 use Webparking\LaravelCash\Tests\TestCase;
 
 final class DeliveryAddressTest extends TestCase
@@ -38,6 +40,28 @@ final class DeliveryAddressTest extends TestCase
         $deliveryAddresses = $deliveryAddress->index();
 
         static::assertEmpty($deliveryAddresses);
+    }
+
+    public function testSave(): void
+    {
+        $this->markTestIncomplete('What is the true identifier here?');
+
+        /** @var DeliveryAddress $deliveryAddress */
+        $deliveryAddress = $this->app->make(DeliveryAddress::class);
+
+        $resource = (new DeliveryAddressResource())->fill([
+            'relationNumber' => '000100',
+            'description' => 'This is a test delivery address.',
+            'name' => 'Test B.V.',
+            'attentionOf' => 'John Doe',
+            'address' => 'Teststraat 12',
+            'postalCodeCity' => '1234AB Test',
+            'country' => 'Nederland',
+            'countryCode' => 'NL',
+            'phoneNumber' => '0612345678',
+            'comments' => 'Dit is een test',
+            'globalLocationNumber' => 'ABCDEFGHIJKLM',
+        ]);
     }
 
     /**
