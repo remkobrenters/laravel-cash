6ccced8246302dbb7a3f21aad4ddb63b85edd688
Dion Gerrese <dion.gerrese@webparking.nl>
2022-02-16 16:48:43 +0100

    Added additional fields to contract and contractType resources

--- src/Resources/ContractType.php
+++ src/Resources/ContractType.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace Webparking\LaravelCash\Resources;
 
 /**
- * @property string $contractType 4100 Contractsoort
- * @property string $description  2001 Omschrijving
+ * @property string $contractType              4100 Contractsoort
+ * @property string $description               2001 Omschrijving
+ * @property string $warrantyPeriodInMonths    4202 Garantietermijn (mnd)
+ * @property string $maintenancePeriodInMonths 2351 Onderh.termijn (mnd)
+ * @property string $billingPeriodInMonths     4252 Fact. termijn (mnd)
+ * @property string $contractInvoiceCostCode   4008 Kostencode contractfactuur
+ * @property string $billingMethod             4104 Factureringsmethode
  */
 class ContractType extends BaseResource implements ResourceInterface
 {
     protected array $mapping = [
-        'description' => 'F2001',
         'contractType' => 'F4100',
+        'description' => 'F2001',
+        'warrantyPeriodInMonths' => 'F4202',
+        'maintenancePeriodInMonths' => 'F2351',
+        'billingPeriodInMonths' => 'F4252',
+        'contractInvoiceCostCode' => 'F4008',
+        'billingMethod' => 'F4104',
     ];
 
     public function getIdField(): string
@@ -28,5 +38,30 @@ class ContractType extends BaseResource implements ResourceInterface
     public function getDescription(): ?string
     {
         return $this->attributes['description'];
+    }
+
+    public function getWarrantyPeriodInMonths(): ?string
+    {
+        return $this->attributes['warrantyPeriodInMonths'];
+    }
+
+    public function getMaintenancePeriodInMonths(): ?string
+    {
+        return $this->attributes['maintenancePeriodInMonths'];
+    }
+
+    public function getBillingPeriodInMonths(): ?string
+    {
+        return $this->attributes['billingPeriodInMonths'];
+    }
+
+    public function getContractInvoiceCostCode(): ?string
+    {
+        return $this->attributes['contractInvoiceCostCode'];
+    }
+
+    public function getBillingMethod(): ?string
+    {
+        return $this->attributes['billingMethod'];
     }
 }
